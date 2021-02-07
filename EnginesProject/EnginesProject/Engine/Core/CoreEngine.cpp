0cd09e5cb605225a24f166b7b97b02b7a4997aa9
iamdatyoung1 <45438168+iamdatyoung1@users.noreply.github.com>
2021-02-07 12:05:30 -0500

    Update CoreEngine.cpp

--- EnginesProject/EnginesProject/Engine/Core/CoreEngine.cpp
+++ EnginesProject/EnginesProject/Engine/Core/CoreEngine.cpp
@@ -2,7 +2,7 @@
 
 std::unique_ptr<CoreEngine> CoreEngine::engineInstance = nullptr;
 
-CoreEngine::CoreEngine() : window(nullptr), isRunning(false)
+CoreEngine::CoreEngine() : window(nullptr), isRunning(false), fps(60), gameInterface(nullptr), currentSceneNum(0)
 {
 }
 
@@ -20,16 +20,29 @@ CoreEngine* CoreEngine::GetInstance()
 
 bool CoreEngine::OnCreate(std::string name_, int width_, int height_)
 {
+	//our debbuger class is complety static so we dont need any pointers we can just output the debugger oncreate
+	//Static allows you to not use any kind of pointer example window->oncreate
+	Debug::OnCreate();
 	window = new Window();
 	//create window
 	if (!window->OnCreate(name_, width_, height_)) {
-		std::cout << "Window failed to initialize" << std::endl;
+		Debug::FatalError("Failed to intialize", "CoreEngine.cpp", __LINE__);
 		//Destroy it to clean up
 		OnDestroy();
 		//return to vaule of false
 		return isRunning = false;
-
 	}
+	//if the gameinterface is not creating the pull up the string game failed and call ondestroy
+	if (gameInterface) {
+		if (!gameInterface->OnCreate()) {
+			Debug::Warning("Gane failed to initialize", "CoreEngine.cpp", __LINE__);
+			OnDestroy();
+			return isRunning = false;
+		}
+	}
+	//Dont need this but it is good to have cause it allows you to know if it worked or not
+	Debug::Info("Everything worked", "CoreEngine.cpp", __LINE__);
+	timer.Start();
 	//if the creation of the winodw works prroperly then return isRunning varrible to true 
 	return isRunning = true;
 }
@@ -38,10 +51,14 @@ void CoreEngine::Run()
 {
 	//Loop isRunning constaly update and render
 	while (isRunning) {
+		timer.UpdateFrameTicks();
+		Update(timer.GetDeltaTime());
+	
 		//always call update before anything for the purpose of always updating time
 		//0.016 is actually 60 frames the reaon is because 1 deived by 60 would give you this number
-		Update(0.016f);
+		
 		Render();
+		SDL_Delay(timer.GetSleepTime(fps));
 	}
 	//if isRunning is not set in motion then just Destroy
 	//dont really need !isrunning but we use it anyways to make it perfectly clear
@@ -50,14 +67,40 @@ void CoreEngine::Run()
 	}
 }
 
-bool CoreEngine::GetIsRunning()
+bool CoreEngine::GetIsRunning() const
 {
 
 	return isRunning;
 }
 
+void CoreEngine::Exit()
+{
+	isRunning = false;
+}
+
+int CoreEngine::GetCurrentScene() const
+{
+	return currentSceneNum;
+}
+
+void CoreEngine::SetGameInterface(GameInterface* gameInterFace_)
+{
+	//this is just allowing us to set it to the pointer that is getting passed through 
+	gameInterface = gameInterFace_;
+}
+
+void CoreEngine::SetCurrentScene(int SceneNum_)
+{
+	currentSceneNum = SceneNum_;
+}
+
 void CoreEngine::Update(const float deltaTime_)
 {
+	//outputs all the numbers on the debugger
+	if (gameInterface) {
+		gameInterface->Update(deltaTime_);
+		std::cout << deltaTime_ << std::endl;
+	}
 
 }
 
@@ -65,6 +108,9 @@ void CoreEngine::Render()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	if (gameInterface) {
+		gameInterface->Render();
+	}
 	//Call Game Render
 	SDL_GL_SwapWindow(window->GetWindow());
 
@@ -72,6 +118,9 @@ void CoreEngine::Render()
 
 void CoreEngine::OnDestroy()
 {
+	//allows destroy from inside out 
+	delete gameInterface;
+	gameInterface = nullptr;
 	delete window;
 	window = nullptr;
 	SDL_Quit();
