3c5f7b82a81f373a815562ea0e7e256983b1d393
iamdatyoung1 <45438168+iamdatyoung1@users.noreply.github.com>
2021-02-07 12:03:21 -0500

    Add files via upload

---
+++ EnginesProject/EnginesProject/GameScene.cpp
@@ -0,0 +1,24 @@
+#include "GameScene.h"
+
+GameScene::GameScene() 
+{
+}
+
+GameScene::~GameScene()
+{
+}
+
+bool GameScene::OnCreate()
+{
+	std::cout << "GameScene" << std::endl;
+
+	return true;
+}
+
+void GameScene::Update(const float deltaTime_)
+{
+}
+
+void GameScene::Render()
+{
+}
