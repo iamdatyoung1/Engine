76722998115186b9188763356eaadbded7f386e4
iamdatyoung1 <45438168+iamdatyoung1@users.noreply.github.com>
2021-02-12 13:18:39 -0500

    Update GameScene.cpp

--- EnginesProject/EnginesProject/GameScene.cpp
+++ EnginesProject/EnginesProject/GameScene.cpp
@@ -1,17 +1,42 @@
 #include "GameScene.h"
-
-GameScene::GameScene() 
+//come back to this
+GameScene::GameScene()  :shape(nullptr)
 {
 }
 
 GameScene::~GameScene()
 {
+	delete shape;
+	shape = nullptr;
 }
 
 bool GameScene::OnCreate()
 {
-	std::cout << "GameScene" << std::endl;
+	Debug::Info("Creating GameScene", "GameScene.cpp", __LINE__);
 
+	Vertex v;
+	std::vector<Vertex> vertexList;
+	//reservers the element
+	//first triangle
+	vertexList.reserve(6);
+	v.position = glm::vec3(0.5f, 0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(-0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(0.5f, -0.5f, 0.0f);
+	vertexList.push_back(v);
+	//second triangle
+	v.position = glm::vec3(-0.5, 0.5, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(-0.5, -0.5, 0.0f);
+	vertexList.push_back(v);
+	v.position = glm::vec3(0.5, 0.5, 0.0f);
+	vertexList.push_back(v);
+
+
+	Model* model = new Model();
+	model->AddMesh(new Mesh(vertexList));
+	shape = new GameObject(model);
 	return true;
 }
 
@@ -21,4 +46,5 @@ void GameScene::Update(const float deltaTime_)
 
 void GameScene::Render()
 {
+	shape->Render();
 }
