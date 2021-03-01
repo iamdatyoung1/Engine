7bcf628eb14ad175a138f76c9c75131bc8e87e12
iamdatyoung1 <45438168+iamdatyoung1@users.noreply.github.com>
2021-03-01 10:40:40 -0500

    Update ColourVertexShader.glsl

--- EnginesProject/ColourVertexShader.glsl
+++ EnginesProject/ColourVertexShader.glsl
@@ -2,15 +2,19 @@
 //This has to be in order
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 normal;
-layout(location = 2) in vec3 texCoords;
+layout(location = 2) in vec2 texCoords;
 layout(location = 3) in vec3 colour;
 
 out vec3 Normal;
 out vec2 TexCoords;
 out vec3 Colour;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main(){
 //The code below with position can only be in vertex
-    gl_Position = vec4(posiition, 1.0f);
+    gl_Position = projection * view *  model * vec4(position, 1.0f);
     Colour = colour;
 }
